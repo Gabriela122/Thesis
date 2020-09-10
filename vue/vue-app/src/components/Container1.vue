32fe91fb9a34ed86ba6ff36b5858e0d03251266c
Gabriela Kosturkiewicz <gabrielakosturkiewicz@gmail.com>
2020-09-10 10:14:07 +0200

    re-created vue app

---
+++ vue/vue-app/src/components/Container1.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div id="container1" class="cont">
+      <h1>Procesy asynchroniczne</h1>
+      <br />
+      <button v-on:click="textDisplay1">Asynchroniczność</button>
+      <div
+        id="tekst1"
+        style="display: none"
+      >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer varius dictum metus quis aliquet. Cras ac convallis nisl, venenatis</div>
+      <br />
+      <br />
+      <div
+        id="tekst2"
+        style="display: none"
+      >ngilla enim vulputate. Nulla facilisi. Aliquam erat volutpat. Morbi ut lacus ac nisi efficitur tristique. Quisque inte</div>
+      <div id="asyncTime"></div>
+      <br />
+    </div>
+    <div>
+      <a href="#container2">
+        <div class="arrow">
+          <p class="arrow-para">V</p>
+        </div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Container1',
+  methods: {
+    async textDisplay1 () {
+      var tekst2 = document.querySelector('#tekst2')
+      var tekst1 = document.querySelector('#tekst1')
+      var t = performance.now()
+      var txt2 = await this.textDisplay2()
+      tekst2.style.display = txt2
+      tekst1.style.display = 'block'
+      var time = performance.now() - t + 'milisekund'
+      document.querySelector('#asyncTime').innerHTML = time
+    },
+    textDisplay2 () {
+      return new Promise(resolve => setTimeout(() => { resolve('block') }, 0))
+    }
+  }
+}
+</script>
---
+++ vue/vue-app/src/components/Container4.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <div id="container4" class="cont"><h1>Animacje</h1>
+            <div id="btns"><!--byl but-->
+                <button class="slideButton" @click="Slide">Play</button>
+            </div>
+            <div class="box red"></div>
+            <div class="box blue"></div>
+            <div class="box lime"></div>
+            <div class="box yellow"></div>
+        <br><br><br><br>Czas trwania animacji
+            <p id="animationTime"></p>
+        </div>
+        <div class="footer"></div>
+    </div>
+</template>
+
+<script>
+import anime from 'animejs'
+var t1
+var t2
+var timeResolution = document.getElementById('animationTime')
+export default {
+  name: 'Container4',
+  methods: {
+    Slide() {
+      anime({
+        targets: 'div.box',
+        translateX: [
+          { value: 200, duration: 500 },
+          { value: 0, duration: 500 }
+        ],
+        rotate: {
+          value: '1turn',
+          easing: 'easeInOutSine'
+        },
+        borderRadius: ['0%', '50%'],
+        easing: 'easeInOutQuad',
+        opacity: '0.3',
+        begin: function () {
+          t1 = performance.now()
+        },
+        complete: function () {
+          t2 = performance.now()
+          timeResolution.innerHTML = t2 - t1 + ' milisekund'
+        },
+        delay: function (el, i) { return i * 1000 }
+      })
+    } 
+  }
+}
+</script>
